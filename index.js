ec688152f2924a040a700d7982cc2f6a3983ab54
Rich Trott <rtrott@gmail.com>
2019-01-29 20:59:22 -0800

    update lodash, use per-method package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const cities = require('./cities.json')
 const diseases = require('./diseases.json');
-const sample = require('lodash/collection/sample');
+const sample = require('lodash.sample');
 
 module.exports = function () {
 	return sample(cities.entries) + ' ' + sample(diseases.entries);
