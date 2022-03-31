d17157dd29140a1ce02aa37cf016ffb09a6fc5d0
Rich Trott <rtrott@gmail.com>
2022-03-31 13:19:56 -0700

    chore: update/remove dependencies (#18)
    BREAKING CHANGE: no longer supports help or version messages

--- test.js
+++ test.js
@@ -1,18 +1,16 @@
 'use strict'
-const test = require('ava')
 const pandemic = require('./')
+const assert = require('assert')
 
-test('returns a random name', function (t) {
-  t.assert(pandemic().length > 0)
-})
+// returns a name
+assert(pandemic().length > 0)
 
-test('returns a two-part name', function (t) {
-  t.assert(pandemic().indexOf(' ') > 0)
-})
+// returns a two-part name
+assert(pandemic().indexOf(' ') > 0)
 
-test('returns a different name each call', function (t) {
-  // Make sure there's variety.
+// returns different names
+{
   const names = [pandemic(), pandemic(), pandemic(), pandemic(), pandemic()]
   const unique = names.filter((val, idx, arr) => arr.indexOf(val) === idx)
-  t.assert(unique.length > 3)
-})
+  assert(unique.length > 3)
+}
