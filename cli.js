94921917b945a21eb28e3da4fbc8d33923b7f4a5
Rich Trott <rtrott@gmail.com>
2022-03-31 18:09:22 +0000

    chore: update/remove dependencies
    BREAKING CHANGE: no longer supports help or version messages

--- cli.js
+++ cli.js
@@ -1,14 +1,5 @@
 #!/usr/bin/env node
 'use strict'
-const meow = require('meow')
 const pandemics = require('./')
-
-meow({
-  help: [
-    'Examples',
-    '  $ dandruff-pinkeye-halitosis',
-    '  Hong Kong Halitosis'
-  ].join('\n')
-})
 
 console.log(pandemics())
